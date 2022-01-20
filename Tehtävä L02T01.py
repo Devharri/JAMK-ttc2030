4ddc3cc48b3c8ac342e607dd772bae416afc85ec
Devharri <devharri@outlook.com>
2022-01-20 16:01:14 +0200

    aloitettu

---
+++ Tehtävä L02T01.py
@@ -0,0 +1,5 @@
+# Tehtävä L02T01
+
+luku1 = len(input("Anna Sukunimesi :"))
+
+print(luku1)
---
+++ Tehtävä L02T02.py
@@ -0,0 +1,4 @@
+# Tehtävä L02T02
+
+etunimi = input("Anna etunimesi :")
+sukunimi = input("Anna sukunimesi: ")
