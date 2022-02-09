acd1dcc2da3861d2c28a55b5da60066621fcfcbe
Devharri <devharri@outlook.com>
2022-02-09 10:23:28 +0200

    Tehty tehtävä

---
+++ L07/Tehtävä L07T01.py
@@ -0,0 +1,30 @@
+# Tehtävä L07T01
+
+class Pelikortti:
+    maa = ""
+    arvo = ""
+
+kortti1 = Pelikortti()
+kortti1.arvo = "2"
+kortti1.maa = "pata"
+print(kortti1.arvo, kortti1.maa)
+
+kortti2 = Pelikortti()
+kortti2.arvo = "3"
+kortti2.maa = "hertta"
+print(kortti2.arvo, kortti2.maa)
+
+kortti3 = Pelikortti()
+kortti3.arvo = "4"
+kortti3.maa = "risti"
+print(kortti3.arvo, kortti3.maa)
+
+kortti4 = Pelikortti()
+kortti4.arvo = "5"
+kortti4.maa = "ruutu"
+print(kortti4.arvo, kortti4.maa)
+
+kortti5 = Pelikortti()
+kortti5.arvo = "6"
+kortti5.maa = "pata"
+print(kortti5.arvo, kortti5.maa)
