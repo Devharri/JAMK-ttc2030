90eabc86beb715f79adc2c301fcba134569082bb
Devharri <devharri@outlook.com>
2022-02-09 15:36:47 +0200

    tehty tehtävä

--- L07/Tehtävä L07T04.py
+++ L07/Tehtävä L07T04.py
@@ -1,1 +1,19 @@
 # Tehtävä L0704
+
+class Mobile:
+    brand = ""
+    model = ""
+    price = 0
+    def __str__(self):
+        return self.brand + " " + self.model + ", " + str(self.price) + "€"
+    def __init__(self, brand = "", model = "", price = 0):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+
+phone1 = Mobile("Samsung", "Galaxy", 349)
+phone2 = Mobile("Apple", "iPhone 12", 899)
+
+print(phone1)
+print(phone2)
