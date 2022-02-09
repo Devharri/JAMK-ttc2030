a5014db55c924823c376bc05a0de191569d56e5f
Devharri <devharri@outlook.com>
2022-02-09 18:24:16 +0200

    tehty tehtävä

--- L08/Tehtävä L08T01.py
+++ L08/Tehtävä L08T01.py
@@ -1,1 +1,14 @@
 # Tehtävä L08T01
+
+nimet = []
+numero = 0
+
+while numero < 10:
+    nimet.append(input("Anna nimi: "))
+    numero += 1
+
+for i in range (len(nimet)):
+    print(nimet[i])
+
+for i in range (len(nimet)):
+    print(nimet[len(nimet)-1-i])
