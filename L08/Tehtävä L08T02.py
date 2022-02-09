5386838c682dde654c4777c228c8cbd6a555166a
Devharri <devharri@outlook.com>
2022-02-09 18:49:04 +0200

    tehty tehtävä

--- L08/Tehtävä L08T02.py
+++ L08/Tehtävä L08T02.py
@@ -1,1 +1,14 @@
 # Tehtävä L08T02
+
+isValueNull = False
+rekkarit = []
+
+while isValueNull == False:
+    teksti = input("Anna rekisterinmero: ")
+
+    if teksti:
+        rekkarit.append(teksti)
+    else:
+        isValueNull = True
+        rekkarit.sort()
+        print(rekkarit)
