55f594b592e0c24e823f7f2cee87d57e92629004
Devharri <devharri@outlook.com>
2022-02-09 10:28:45 +0200

    Tehty tehtävä

---
+++ L07/Tehtävä L07T02.py
@@ -0,0 +1,20 @@
+# Tehtävä L07T02
+
+from unicodedata import name
+
+
+class Human:
+    name = ""
+    age = 0
+    def __str__(self):
+        return "Nimi: " + self.name + ", " + "ikä: " + str(self.age)
+
+ihminen1 = Human()
+ihminen1.name = "Adam"
+ihminen1.age = 19
+print(ihminen1)
+
+ihminen2 = Human()
+ihminen2.name = "Eve"
+ihminen2.age = 18
+print(ihminen2)
