9d3b3faa48b37166a4de9fcdc677b0c84c9ffdbe
Devharri <devharri@outlook.com>
2022-02-10 11:31:23 +0200

    Tehty tehtävä

--- L09/Tehtävä L09T01.py
+++ L09/Tehtävä L09T01.py
@@ -1,6 +1,17 @@
 # Tehtävä L09T01
 
+isValueNull = False
+thislist = []
+while isValueNull == False:
+    nimi = input("Anna sukunimi: ")
+    
+    if nimi:
+        thislist.append(nimi)
+    else:
+        isValueNull = True
 
-#..
-
-import random
+filename = "testi.txt"
+file = open(filename, "w")
+for x in thislist:
+    file.write(x + "\n")
+file.close()
