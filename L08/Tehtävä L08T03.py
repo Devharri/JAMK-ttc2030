cdcd3d0ef5e2a5f3588ae0877ca6d4b37852aad8
Devharri <devharri@outlook.com>
2022-02-10 09:29:51 +0200

    tehty tehtävä

--- L08/Tehtävä L08T03.py
+++ L08/Tehtävä L08T03.py
@@ -1,1 +1,17 @@
 # Tehtävä L08T03
+
+isValueNull = False
+numCount = 0
+numAvg = 0.0
+arvosanat = []
+
+while isValueNull == False:
+    numero = input("Anna arvosana: ")
+
+    if numero:
+        arvosanat.append(int(numero))
+        numCount += 1
+    else:
+        isValueNull = True
+        numAvg = (sum(arvosanat) / numCount)
+        print("Keskiarvo: " + str(numAvg) + ", lukumäärä: " + str(numCount))
