c005de37b48c0a4c577ca45040f1db6d52ba3e7c
Devharri <devharri@outlook.com>
2022-02-10 10:52:47 +0200

    tehty tehtävä

--- L08/Tehtävä L08T04.py
+++ L08/Tehtävä L08T04.py
@@ -1,2 +1,25 @@
 # Tehtävä L08T04
 
+autot = {}
+lista = []
+laskuri = 0
+
+while laskuri < 3:
+    reknumero = input("Anna rekisterinumero: ")
+    valmistaja = input("Anna auton valmistaja: ")
+    
+    if reknumero:
+        if valmistaja:
+            laskuri += 1
+            autot[reknumero] = valmistaja
+        else:
+            print("Input was empty. Give valid input.")
+
+    else:
+        print("Input was empty. Give valid input.")
+
+for x, y in autot.items():
+  lista.append((x+ " " + y))
+lista.sort()
+for nimi in lista:
+    print(nimi)
