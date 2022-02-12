8637107010c36c1e037c60c0ae3d0d284da4b9a4
Devharri <devharri@outlook.com>
2022-02-12 10:43:21 +0200

    tehty tehtävä

--- L10/Tehtävä L10T01.py
+++ L10/Tehtävä L10T01.py
@@ -1,1 +1,12 @@
 # Tehtävä L10T01
+
+balance = 2000
+
+print("Bank account balance: " + str(balance))
+
+luku1 = float(input("How many euros will be added to the balance? "))
+luku2 = float(input("How many cents will be added to the balance? "))
+
+luku3 = balance + luku1+ (luku2/100)
+
+print("Bank account balance: " + str(luku3))
