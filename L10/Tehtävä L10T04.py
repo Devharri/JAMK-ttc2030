907e29480b078472402d7ef01a45cbf30a01d7c2
Devharri <devharri@outlook.com>
2022-02-13 14:49:38 +0200

    tehty tehtävä

---
+++ L10/Tehtävä L10T04.py
@@ -0,0 +1,19 @@
+# Tehtävä L10T04
+
+nimet = []
+
+nimet.append("Ykä")
+nimet.append("Eka")
+nimet.append("Ekku")
+nimet.append("Musti")
+
+try:
+    indeksi = int(input("Monettako indeksiä muutetaan: "))
+    teksti = input("Anna teksti: ")
+    print("Vanha nimi: " + nimet[indeksi])
+    nimet[indeksi] = teksti
+    print("Uusi nimi: " + nimet[indeksi])
+except IndexError:
+    print("List index out of range.")
+except:
+    print("Something else went wrong,")
