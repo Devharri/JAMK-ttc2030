625ae9f841f7837aa24166e1424875e3a2143796
Devharri <devharri@outlook.com>
2022-02-15 09:39:15 +0200

    tehty tehtävä

--- L10/Tehtävä L10T05.py
+++ L10/Tehtävä L10T05.py
@@ -1,1 +1,11 @@
 # Tehtävä L10T05
+
+try:
+    filename = "C:\L09T01.txt"
+    file = open(filename, "w")
+    file.write("Tekstiä" + "\n")
+    file.close()
+except PermissionError:
+    print("Permission denied. Try another location.")
+except:
+    print("Something else went wrong.")
